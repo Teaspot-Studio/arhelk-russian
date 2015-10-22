1b22eef318c39b537bef33ed527d906f09922fc6
Anton Gushcha <ncrashed@gmail.com>
2015-10-23 01:21:54 +0300

    Implement base Lemma type and adapt testing

--- test/Test/Arhelk/Russian/Lemma.hs
+++ test/Test/Arhelk/Russian/Lemma.hs
@@ -2,9 +2,12 @@ module Test.Arhelk.Russian.Lemma(
     testModule
   ) where 
 
+import Arhelk.Russian.Lemma
+import Data.List as L
+import Data.Monoid
+import Data.Text as T
 import qualified Test.Framework as TF
 import Test.Framework.Providers.HUnit
-import Data.Text 
 import Test.HUnit
 
 testModule :: TF.Test
@@ -14,5 +17,17 @@ testModule = TF.testGroup "Lemmanization" [
   , testCase "Root сказ" $ isSameRoot ["сказать", "подсказать", "пересказать", "сказал", "сказала", "пересказала", "сказало", "подсказало", "досказал"]
   ]
 
+-- | Succedes only when all roots of words are equal
 isSameRoot :: [Text] -> Assertion
-isSameRoot ws = assertFailure "Unimplemented"
+isSameRoot ws = assertBool ("Roots are not equal for [" <> unpack (T.intercalate ", " ws) <> "]") $ allSame $ lemmaRoot . lemmanize <$> ws
+
+-- | Returns True if all elements in list are equal each other
+-- Note: allSame [] == True
+allSame :: Eq a => [a] -> Bool
+allSame = snd . L.foldl' go (Nothing, True)
+  where
+    go (_ , False) _ = (Nothing, False) 
+    go (Nothing, True) w = (Just w, True)
+    go (Just w1, True) w2 
+      | w1 == w2 = (Just w1, True)
+      | otherwise = (Nothing, False)

b234ff8a486d0160fde2285ab47019301c977eb9
Anton Gushcha <ncrashed@gmail.com>
2015-10-23 01:21:54 +0300

    Implement base Lemma type and adapt testing

--- src/Arhelk/Russian/Lemma.hs
+++ src/Arhelk/Russian/Lemma.hs
@@ -1,4 +1,41 @@
 module Arhelk.Russian.Lemma(
-
+    SpeachPart(..)
+  , Lemma(..)
+  , lemmanize
   ) where 
 
+import Data.Text 
+
+-- | Describes possible part of speach in Russian
+data SpeachPart = 
+    Substantive
+  | Adjective
+  | Numeral
+  | Pronoun
+  | Verb
+  | Adverb
+  | Preposition
+  | Conjunction
+  | GrammarParticle
+  | Interjection
+  | Participle
+  | Transgressive
+  deriving (Eq, Ord, Enum, Show)
+
+-- | Parsed word with detached prefixes, postfixes and determinied speach part
+data Lemma = Lemma {
+-- | Initial full word
+  lemmaSource :: Text
+-- | Semanitc root
+, lemmaRoot :: Text
+-- | Semantic prefixes
+, lemmaPrefixes :: [Text]
+-- | Semantic postfixes
+, lemmaPostfixes :: [Text]
+-- | Determinied part of speach
+, lemmaSpeachPart :: Maybe SpeachPart
+}
+
+-- | Calculate work lemma
+lemmanize :: Text -> Lemma
+lemmanize = error "unimplemented"

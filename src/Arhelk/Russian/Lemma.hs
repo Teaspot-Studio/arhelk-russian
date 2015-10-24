08caec188a84f010e4a1d120fef46638579c5eb6
Anton Gushcha <ncrashed@gmail.com>
2015-10-24 19:21:55 +0300

    Adjective rules

--- src/Arhelk/Russian/Lemma.hs
+++ src/Arhelk/Russian/Lemma.hs
@@ -2,6 +2,7 @@ module Arhelk.Russian.Lemma(
     Lemma(..)
   , lemmanize
   , substantive
+  , adjective
   , genderByDeclension
   , module X
   ) where 
@@ -51,7 +52,7 @@ substantive w = do
   propose substCase Genitivus $ do 
     propose substQuantity GrammarSingle $ do 
       when (w `endsWith` ["ы", "и"]) $ imply substDeclension FirstDeclension
-      when (w `endsWith` ["а", "я"]) $ imply substDeclension SecondDeclension
+      when (w `endsWith` ["а", "я", "у", "ю"]) $ imply substDeclension SecondDeclension
       when (w `endsWith` ["и"]) $ imply substDeclension ThirdDeclension
     propose substQuantity GrammarMultiple $ do 
       when (w `endsWith` ["", "ов", "ев", "ей"]) implyNothing
@@ -67,10 +68,18 @@ substantive w = do
   propose substCase Accusativus $ do 
     propose substQuantity GrammarSingle $ do 
       when (w `endsWith` ["у", "ю"]) $ imply substDeclension FirstDeclension
-      when (w `endsWith` ["о", "е"]) $ imply substDeclension SecondDeclension
+      when (w `endsWith` ["", "о", "е", "а", "я", "у", "ю"]) $ imply substDeclension SecondDeclension
       when (w `endsWith` [""]) $ imply substDeclension ThirdDeclension
     propose substQuantity  GrammarMultiple $ do 
-      when (w `endsWith` ["ами", "ями"]) implyNothing
+      when (w `endsWith` ["", "ы", "и", "а", "я", "ов", "ев", "ей"]) implyNothing
+
+  propose substCase Ablativus $ do 
+    propose substQuantity GrammarSingle $ do 
+      when (w `endsWith` ["ой", "ою", "ей", "ею"]) $ imply substDeclension FirstDeclension
+      when (w `endsWith` ["ом", "ем"]) $ imply substDeclension SecondDeclension
+      when (w `endsWith` ["ью"]) $ imply substDeclension ThirdDeclension
+    propose substQuantity GrammarMultiple $ do 
+      when (w `endsWith` ["ами", "ями", "ми"]) implyNothing
 
   propose substCase Praepositionalis $ do 
     propose substQuantity GrammarSingle $ do 
@@ -89,3 +98,60 @@ genderByDeclension = implyMap $ \s -> case view substDeclension s of
   Nothing -> [s]
   where
     upd s v = set substGender (Just v) s
+
+adjective :: Text -> Rule AdjectiveProperties
+adjective w = do 
+  propose adjCase Nominativus $ do 
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ой", "ый", "ий"]) $ imply adjGender GrammarMale
+      when (w `endsWith` ["оe", "ee"]) $ imply adjGender GrammarNeuter
+      when (w `endsWith` ["ая", "яя"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ые", "ие"]) implyNothing
+
+  propose adjCase Genitivus $ do 
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ого", "eго"]) $ do 
+        imply adjGender GrammarMale
+        imply adjGender GrammarNeuter
+      when (w `endsWith` ["ой", "ей"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ых", "их"]) implyNothing
+
+  propose adjCase Dativus $ do 
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ому", "eму"]) $ do 
+        imply adjGender GrammarMale
+        imply adjGender GrammarNeuter
+      when (w `endsWith` ["ой", "ей"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ым", "им"]) implyNothing
+
+  propose adjCase Accusativus $ do 
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ой", "ый", "ий"]) $ imply adjGender GrammarMale
+      when (w `endsWith` ["оe", "ee"]) $ imply adjGender GrammarNeuter
+      when (w `endsWith` ["ого", "eго"]) $ do 
+        imply adjGender GrammarMale
+        imply adjGender GrammarNeuter
+      when (w `endsWith` ["ую", "юю"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ые", "ие", "ых", "их"]) implyNothing
+        
+  propose adjCase Ablativus $ do
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ым", "им"]) $ do 
+        imply adjGender GrammarMale
+        imply adjGender GrammarNeuter
+      when (w `endsWith` ["ой", "ою", "ей", "ею"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ыми", "ими"]) implyNothing
+
+  propose adjCase Praepositionalis $ do
+    propose adjQuantity GrammarSingle $ do
+      when (w `endsWith` ["ом", "eм"]) $ do 
+        imply adjGender GrammarMale
+        imply adjGender GrammarNeuter
+      when (w `endsWith` ["ой", "ей"]) $ imply adjGender GrammarFemale
+    propose adjQuantity GrammarMultiple $ do
+      when (w `endsWith` ["ых", "их"]) implyNothing

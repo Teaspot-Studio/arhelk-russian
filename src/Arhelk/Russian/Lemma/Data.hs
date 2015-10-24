1fe25611cfa3cd166bd1feb1d81703a597918824
Anton Gushcha <ncrashed@gmail.com>
2015-10-24 19:21:55 +0300

    Adjective rules

--- src/Arhelk/Russian/Lemma/Data.hs
+++ src/Arhelk/Russian/Lemma/Data.hs
@@ -113,6 +113,32 @@ instance TextShow Animacy where
     AnimateNoun -> "одушвл."
     InanimateNoun -> "неодушвл."
 
+-- | Разряд прилагательного
+data AdjectiveCategory = 
+    QualitiveAdjective -- ^ Качественное
+  | ComparativeAdjective -- ^ Относительное
+  | PossessiveAdjective -- ^ Притяжательное
+  deriving (Eq, Ord, Enum, Show, Bounded)
+
+instance TextShow AdjectiveCategory where 
+  showb v = case v of 
+    QualitiveAdjective -> "качеств."
+    ComparativeAdjective -> "сравн."
+    PossessiveAdjective -> "притяж."
+
+-- | Степень сравнения
+data AdjectiveDegree =
+    PositiveDegree -- ^ Положительная степерь
+  | ComparitiveDegree -- ^ Сравнительная степень
+  | SuperlativeDegree -- ^ Превосходная степень
+  deriving (Eq, Ord, Enum, Show, Bounded)
+
+instance TextShow AdjectiveDegree where 
+  showb v = case v of 
+    PositiveDegree -> "полож. степень"
+    ComparitiveDegree -> "сравн. степень"
+    SuperlativeDegree -> "превосх. степень"
+
 -- | Substantive morphological properties
 data SubstantiveProperties = SubstantiveProperties {
   _substAppellativity :: Maybe Appellativity
@@ -152,4 +178,40 @@ instance TextShow SubstantiveProperties where
     , maybe "" showb _substGender
     , maybe "" showb _substQuantity
     , maybe "" showb _substCase
+    ]
+
+data AdjectiveProperties = AdjectiveProperties {
+  _adjGender :: Maybe GrammarGender
+, _adjCase :: Maybe GrammarCase
+, _adjQuantity :: Maybe GrammarQuantity
+, _adjShort :: Maybe Bool
+, _adjCategory :: Maybe AdjectiveCategory
+}
+
+$(makeLenses ''AdjectiveProperties)
+
+instance Monoid AdjectiveProperties where 
+  mempty = AdjectiveProperties {
+    _adjGender = Nothing
+  , _adjCase = Nothing
+  , _adjQuantity = Nothing
+  , _adjShort = Nothing
+  , _adjCategory = Nothing
+  }
+
+  mappend a b = AdjectiveProperties {
+    _adjGender = getFirst $ First (_adjGender a) <> First (_adjGender b)
+  , _adjCase = getFirst $ First (_adjCase a) <> First (_adjCase b)
+  , _adjQuantity = getFirst $ First (_adjQuantity a) <> First (_adjQuantity b)
+  , _adjShort = getFirst $ First (_adjShort a) <> First (_adjShort b)
+  , _adjCategory = getFirst $ First (_adjCategory a) <> First (_adjCategory b)
+  }
+
+instance TextShow AdjectiveProperties where
+  showb AdjectiveProperties{..} = unwordsB [
+      maybe "" showb _adjGender
+    , maybe "" showb _adjCase
+    , maybe "" showb _adjQuantity
+    , maybe "" showb _adjShort
+    , maybe "" showb _adjCategory
     ]
--- test/Test/Arhelk/Russian/Lemma.hs
+++ test/Test/Arhelk/Russian/Lemma.hs
@@ -3,18 +3,22 @@ module Test.Arhelk.Russian.Lemma(
   ) where 
 
 import Arhelk.Russian.Lemma
+import Arhelk.Russian.Rule
 import Data.List as L
 import Data.Monoid
 import Data.Text as T
 import qualified Test.Framework as TF
 import Test.Framework.Providers.HUnit
 import Test.HUnit
+import TextShow 
 
 testModule :: TF.Test
 testModule = TF.testGroup "Lemmanization" [
     testCase "Root дерев" $ isSameRoot ["дерево", "деревья", "дерева", "деревьев", "дереве"]
   , testCase "Root красн" $ isSameRoot ["красный", "красная", "красное", "красные", "красных", "красного", "красной", "красному", "красным", "красном"]
   , testCase "Root сказ" $ isSameRoot ["сказать", "подсказать", "пересказать", "сказал", "сказала", "пересказала", "сказало", "подсказало", "досказал"]
+  , testCase "Substantive" $ assertFailure $ unpack $ showt $ runRule $ genderByDeclension $ substantive "деревьев"
+  , testCase "Adjective" $ assertFailure $ unpack $ showt $ runRule $ adjective "красный"
   ]
 
 -- | Succedes only when all roots of words are equal
